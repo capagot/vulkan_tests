265ef95c20c86c1822e2dc04de681fc5c7e2423d
Christian <christian.azambuja@gmail.com>
2019-01-24 23:25:35 -0300

    First Vulkan/GLFW3 build test using CMake.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+
+int main() {
+    glfwInit();
+    glfwTerminate();
+    return 0;
+}
